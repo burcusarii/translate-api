dbbb9a47858f52173fea48939ad5992c777896a9
Burcu Irem Sari <burcu.sari5334@gmail.com>
2023-04-01 13:49:06 +0300

    dil seçimi eklendi ve api üzerinden veri alma dinamik hale getirildi.

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const input_text = document.getElementById("input-text")
 const output_text = document.getElementById("output-text")
 const translate_btn = document.getElementById("translate-btn")
+const text_from = document.getElementById("text-from");
+const text_to = document.getElementById("text-to");
 const myKey = config.my_key;
+
 class Request{
 
     get(url, options){
@@ -23,12 +26,13 @@ const options = {
 };
 
 translate_btn.addEventListener("click", function() {
-    let url = `https://nlp-translation.p.rapidapi.com/v1/translate?text=${input_text.value}&to=tr&from=en`
+    let url = `https://nlp-translation.p.rapidapi.com/v1/translate?text=${input_text.value}&to=${text_to.value}&from=${text_from.value}`
 
     istek.get(url, options)
     .then(response => {
-        console.log(response)
-        output_text.value = response.translated_text.tr})
+        const translated_text = response.translated_text
+        output_text.value = Object.values(translated_text);
+     } )
     .catch(err => console.log(err));
 });
 
